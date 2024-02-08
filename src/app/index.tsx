0e18f487469b7cf8a0446dd734afe12fc8b6b305
Gustavo Cintrão <guga@MacBook-Pro---Guga.local>
2024-02-08 00:57:22 -0300

    feat: aula 1 - nlw expert

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Home() {
+  return (
+    <View className="flex-1 pt-8">
+      <Text className="text-white font-heading">AOOOOOBAAAA</Text>
+    </View>
+  );
+}
